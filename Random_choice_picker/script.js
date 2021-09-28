7996619f3fe15f7f64baf25ca96ec81be79e15b5
Pulatshya <pulatshyashukla@gmail.com>
2021-09-28 17:45:56 +0530

    initial commit

---
+++ Random_choice_picker/script.js
@@ -0,0 +1,36 @@
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+  if(e.key === 'Enter'){
+    setTimeout(() =>{
+      e.target.value = '';
+    }, 10)
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  
+  const tags = input.split(",").filter((tag) => tag.trim() !== "").map((tag) => tag.trim()
+  
+  );
+  
+  
+  console.log(tags);
+  
+  tagsEl.innerHTML = '';
+  
+  tags.forEach(tag => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
+function randomSelect(){
+  console.log(123)
+}

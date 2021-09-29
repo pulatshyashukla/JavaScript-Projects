a6899ada2e38370760fd0d957acbc7762ba07df7
Pulatshya <pulatshyashukla@gmail.com>
2021-09-29 22:23:54 +0530

    'second_commit'

--- Random_choice_picker/script.js
+++ Random_choice_picker/script.js
@@ -32,5 +32,37 @@ function createTags(input) {
   });
 }
 function randomSelect(){
-  console.log(123)
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+
+    highlightTag(randomTag)
+
+    setTimeout(() => {
+      unhighlightTag(randomTag)
+    }, 100)
+  }, 100)
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+
+      highlightTag(randomTag)
+
+
+    }, 100)
+
+  }, times * 100)
 }
+function pickRandomTag(){
+  const tags = document.querySelectorAll(".tag")
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+function highlightTag(tag) {
+  tag.classList.add("highlight")
+}
+ function unhighlightTag(tag) {
+   tag.classList.remove("highlight")
+ }
